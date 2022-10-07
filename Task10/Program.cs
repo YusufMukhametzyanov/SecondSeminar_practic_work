1071a0de02aa6c7d8d85938d93dd9600c8d009f9
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-07 14:13:30 +0500

    Add result for task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+void Task10()
+{
+    int a = (new Random()).Next(100, 1000);
+    Console.WriteLine($"Трёхзначное число: {a}");
+    string b = a.ToString();
+    int result = int.Parse(b[1].ToString());
+    Console.WriteLine($"Второе число: {result}");
+}
+
+Task10();
